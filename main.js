809db78bf59068c89df0fb25546436648c9ab238
Arun Lal <arunlalkp.kp@gmail.com>
2019-06-23 16:26:07 +0530

    header caption change

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 var scroll = new SmoothScroll('a[href*="#"]');
+
+$(document).ready(function(){
+
+    
+    var words = [
+        'Web',
+        'App'
+      ], i = 0;
+      
+      setInterval(function () {
+          $('#changeText').fadeOut(400, function() {
+            $(this).text( words[ (i === words.length - 1) ? i = 0 : i += 1 ] ).fadeIn(400);
+        });
+      }, 3000);
+
+
+
+
+})
